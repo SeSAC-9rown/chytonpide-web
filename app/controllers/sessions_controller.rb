a4f9b5d147049f15e30528c9d943c952a1a4de81
kbs-kbs <yaglus58@gmail.com>
2025-11-24 14:51:25 +0900

    path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
 	def new; end
-
+	
 	def create
 		auth = request.env["omniauth.auth"]
 		user = User.find_by(provider: auth.provider, uid: auth.uid)
-
+		
 		if user
 			session[:user_id] = user.id
 			redirect_to root_path
@@ -12,8 +12,8 @@ class SessionsController < ApplicationController
 			session[:omniauth_provider] = auth.provider
 			session[:omniauth_uid] = auth.uid
 			session[:omniauth_email] = auth.info.email
-			session[:omniauth_avatar] = auth.info.image
-			redirect_to new_profile_path
+			session[:omniauth_avatar_url] = auth.info.image
+			redirect_to new_user_path
 		end
 	end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,49 @@
 class UsersController < ApplicationController
+	def new
+		redirect_to root_path and return unless omniauth_present?
+		
+		@user = User.new(
+			email: session[:omniauth_email],
+			social_avatar_url: session[:omniauth_avatar_url]
+		)
+	end
+
+	def create
+		redirect_to root_path and return unless omniauth_present?
+		
+		user = User.new(
+			email: session[:omniauth_email],
+			provider: session[:omniauth_provider],
+			uid: session[:omniauth_uid],
+			name: user_params[:name],
+			avatar_url: user_params[:avatar_url],
+			social_avatar_url: session[:omniauth_avatar_url]
+		)
+		
+		if user.save
+			session[:user_id] = user.id
+			clear_omniauth_session
+			redirect_to root_path
+		else
+			@user = user
+			render :new
+		end
+	end
+	
+	private
+	
+	def user_params
+		params.require(:user).permit(:name, :avatar_url)
+	end
+	
+	def omniauth_present?
+		session[:omniauth_provider].present? && session[:omniauth_uid].present?
+	end
+
+	def clear_omniauth_session
+		session.delete(:omniauth_provider)
+		session.delete(:omniauth_uid)
+		session.delete(:omniauth_email)
+		session.delete(:omniauth_avatar_url)
+	end
 end
