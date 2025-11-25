e3b3fd9c2008d1dee81e1ade584192185f84d4e3
kbs-kbs <yaglus58@gmail.com>
2025-11-26 05:49:14 +0900

    mobile

--- app/controllers/sensor_data_controller.rb
+++ app/controllers/sensor_data_controller.rb
@@ -2,22 +2,24 @@ class SensorDataController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:create]
 
     def create
-        @sensor_data = SensorData.new(
+        @sensor_datum = SensorDatum.new(
             serial: params[:serial],
             temperature: params[:temperature],
             humidity: params[:humidity],
             illuminance: params[:illuminance]
         )
 
-        if @sensor_data.save
+        if @sensor_datum.save
             # Action Cable로 실시간 브로드캐스트
             ActionCable.server.broadcast(
                 "sensor_data_channel",
-                serial: @sensor_data.serial,
-                temperature: @sensor_data.temperature,
-                humidity: @sensor_data.humidity,
-                illuminance: @sensor_data.illuminance,
-                timestamp: @sensor_data.created_at
+                {  # 여기에 중괄호 추가!
+                    serial: @sensor_datum.serial,
+                    temperature: @sensor_datum.temperature,
+                    humidity: @sensor_datum.humidity,
+                    illuminance: @sensor_datum.illuminance,
+                    timestamp: @sensor_datum.created_at
+                }
             )
             render json: { status: "success" }, status: :created
         else
